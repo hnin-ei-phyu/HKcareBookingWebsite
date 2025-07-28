be958b1f78ecf66f4a761778fbc984337c9a8133
hnin-ei-phyu <nanghnineiphyu2001@gmail.com>
2025-07-28 18:25:02 +0700

    added login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [state, setState] = useState('Sign up')
+  
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name,setName] = useState('')
+
   return (
-    <div>
+    <form action="">
       
-    </div>
+    </form>
   )
 }
 
